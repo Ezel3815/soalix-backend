45fd25bb4cf25e2d172e1e1e2ba148de044b14c5
Ezel3815 <skukzleenii@gmail.com>
2026-09-09 13:03:37 +0300

    Add search user functionality in users controller

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -86,6 +86,17 @@ export class UsersController {
 
     @DRole()
     @Get(":id")
+    @DAuth()
+     @Get("search")
+async search(@DUser() user: User, @Query("q") q: string) {
+    return await this.service.searchUsers(q, user.id);
+}
+
+@DRole()
+@Get(":id")
+async readOne(@Param("id", ParseIntPipe) id: number) {
+    return await this.service.readOne(id);
+}
     async readOne(@Param("id", ParseIntPipe) id: number) {
         return await this.service.readOne(id);
     }
